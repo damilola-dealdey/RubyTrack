ed08143d1be107f1d12dc0e9a9e0a7353814d7d3
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-11-15 13:35:48 +0100

    Ex 3 Done

---
+++ Ex3/bin/main.rb
@@ -0,0 +1,13 @@
+def fibonnaci
+  yield 0, 1
+end
+
+
+fibonnaci do |n1 , n2|
+  puts 'Let the series begin ...  below'
+  puts n1
+  begin
+    puts n2
+    n1, n2 = n2, (n2 + n1)
+  end while n2 <= 1000
+end
