2d3259d2211f19ec699c92916fe6617482fc34c4
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-11-15 15:13:48 +0100

    Ex5 done

---
+++ Ex5/bin/main.rb
@@ -0,0 +1,35 @@
+class Customer
+  @@Counter = 0
+  attr_accessor :name, :account_no, :balance 
+  def initialize(name)
+    @@Counter += 1
+    p name
+    @name = name
+    @account_no = @@Counter
+    @balance = Float(1000)
+  end
+
+  def to_s
+    "#{self.name} with Account No. #{self.account_no} has #{self.balance} left."
+  end
+
+  def deposit(amount)
+    @balance += Float(amount)
+    puts self
+  end
+
+  def withdraw(amount)
+    @balance -= Float(amount)
+    puts self
+  end
+
+  def transfer(other_customer, amount)
+    self.withdraw(amount)
+    other_customer.deposit(amount)
+  end
+
+end
+
+john = Customer.new('John Smith')
+paul = Customer.new('Paul Smith')
+john.transfer(paul,150);
