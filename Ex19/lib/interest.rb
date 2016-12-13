6c6e33dd7c43c6012b6f61286582cc1892a2108b
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-12-13 09:57:52 +0100

    All DOne

--- Ex19/lib/interest.rb
+++ Ex19/lib/interest.rb
@@ -12,6 +12,10 @@ class Interest
     si_i_only = @principal * @@rate * @time
     ci = @principal * ((1 + @@rate / n ) ** (n * @time))
     ci_i_only = ci - @principal
+    display_result(si, si_i_only , ci, ci_i_only)
+  end
+
+  def display_result(si,si_i_only,ci,ci_i_only)
     puts "Principal : #{@principal} Money\nTime : #{@time} year(s)\nRate : #{@@rate}\%"
     puts "Calculating simple interest ..."
     puts "Simple Interest\nTotal Amount : #{si} money\nInterest : #{si_i_only} money"
